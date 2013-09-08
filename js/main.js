b1aa595bce08f507c3b322c5ab686fa2b3496a21
Petja <petja@petjatouru.com>
2013-09-08 14:05:37 +0300

    Basic, basic and more basic stuff

--- js/main.js
+++ js/main.js
@@ -4,11 +4,59 @@ $(function(){
 	
 	function writeText(text,x,y,font,color,alignment){
 		ctx.fillStyle = color || "#FFF";
-		ctx.font = font || "32px sans-serif";
+		ctx.font = font || "32px sans-serif,Arial";
 		ctx.textAlign = alignment || "left";
 		ctx.fillText(text,x,y);
 	}
 	
-	// Draw logo
-	writeText("Reckless Runners",canvas.width/2,canvas.height/2-64,"bold 128px sans-serif,sans,Arial",undefined,"center");
+	// Variables
+	gameStatus = "2.0";
+	
+	// 1st number	2nd number	Mean
+	////////////////////////////////////////
+	// 0			0			Main Menu
+	// 1			0			In game
+	// 2			0			Studio logo
+	// 2			1			Game logo
+	// 2			2			Loading screen
+	///////////////////////////////////////
+	
+	// Run drawScreen() every 50ms (20 times per second)
+	setInterval(function(){
+		drawScreen();
+	},50);
+	
+	function drawScreen(){
+		// Clear the screen
+		ctx.beginPath();
+		ctx.rect(0,0,canvas.width,canvas.height);
+		ctx.fillStyle = "#000";
+		ctx.fill();
+		ctx.closePath();
+		
+		switch(gameStatus){
+			case "2.0":
+				// Draw a studio logo
+				writeText("Warped Matter",canvas.width/2,canvas.height/2-64,"192px 'Flavors'","#FFF","center");
+				writeText("TM",canvas.width/10*7,canvas.height/2-256,"24px 'Oxygen'","#FFF","center");
+				console.log("jjj");
+			break;
+			case "2.1":
+				// Draw a game logo
+				writeText("Bad For Business",canvas.width/2,canvas.height/2-64,"192px 'Flavors'","#FFF","center");
+				writeText("TM",canvas.width/10*7,canvas.height/2-256,"24px 'Oxygen'","#FFF","center");
+			break;
+			case "2.2":
+				// Loading screen
+				writeText("Loading",canvas.width/2,canvas.height/2-16,"32px 'Oxygen'","#FFF","center");
+			break;
+		}
+	}
+	
+	setTimeout(function(){
+		gameStatus = "2.1";
+		setTimeout(function(){
+			gameStatus = "2.2";
+		},3000);
+	},3000);
 });
